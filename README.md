e1911ca784fb824b70aeadab3c97abc265bf8614
Mitja Felicijan <mitja.felicijan@gmail.com>
2023-12-03 21:37:07 +0100

    Added line and pie charts and some cleanup

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Simple SVG Sparklines
 
-![Preview](https://github.com/mitjafelicijan/sparkline/assets/296714/bffe023d-4e9a-461c-976c-7e78b296b14c)
+![Preview](https://github.com/mitjafelicijan/sparklines/assets/296714/d60ec13e-fb0c-42cc-89c1-bab270b7571f)
 
-Add `sparkline.js` to your HTML file and use proper data attributes and that is about it.
+Add `sparklines.js` to your HTML file and use proper data attributes and that is about it.
+
+## Basic usage
+
+```html
+<div data-sparkline="true" data-points="6,2,3,4,5" data-width="300" data-height="50" data-gap="15" data-colors="#ff0000"></div>
+<script src="sparklines.js" defer></script>
+```
+
+Check `kitchensink.html` for more examples.
+
+> To create minified version of `sparklines.js` use `npx minify sparklines.js > sparklines.min.js`.
+
+## Bar charts
 
 | Attribute | Description | Default Value |
 |-----------|-------------|---------------|
@@ -11,15 +24,46 @@ Add `sparkline.js` to your HTML file and use proper data attributes and that is 
 | data-width | Width of the container in px | 100 |
 | data-height | Height of the container in px | 30 |
 | data-gap | Gap between the bars in px | 5 |
-| data-color | Color of the bars in HEX or color name | gray |
-
-**Example**
+| data-colors | List of colors of the bars in HEX or color name | gray |
 
 ```html
-<div data-sparkline="true" data-points="6,2,3,4,5" data-width="300" data-height="50" data-gap="15" data-color="#ff0000"></div>
-<script src="sparkline.js" defer></script>
+<div data-sparkline="true" data-points="14,19,7,9,18,20,17" data-colors="white,blue"></div>
+<div data-sparkline="true" data-points="18,13,19,14,14,3,4,8,7,9,2,8,0,20,7,3,2,5,12,15" data-width="300" data-height="50" data-gap="5"></div>
+<div data-sparkline="true" data-points="16,2,5,5,20,11,12,19,17,18,13,8,3,5,14,19,6,1,12,4" data-width="100" data-height="50" data-gap="2" data-colors="purple"></div>
+<div data-sparkline="true" data-points="20,14,4,3,17,2,8,12,19,11,6,17,5,8,13,12,17,9,1,0" data-width="300" data-height="50" data-gap="15" data-colors="#ffff00"></div>
+<div data-sparkline="true" data-points="4,8,10,3,16,15,0,0,8,18,11,14,14,0,11,14,16,14,15,6" data-width="200" data-height="50"></div>
+<div data-sparkline="true" data-points="10,4,10,15,18,10,12,8,11,4,13,14,6,18,17,12,0,13,14,3" data-width="200" data-height="50"></div>
 ```
 
-Check `index.html` for more examples.
+## Line charts
 
-To create minified version of `sparkline.js` use `npx minify sparkline.js > sparkline.min.js`.
+| Attribute | Description | Default Value |
+|-----------|-------------|---------------|
+| data-sparkline | If sparkline is enabled on this element | None |
+| data-points | List of numbers / data | [] |
+| data-width | Width of the container in px | 100 |
+| data-height | Height of the container in px | 30 |
+| data-stroke-width | Size of the line in px | 2 |
+| data-colors | Color of the line in HEX or color name | gray |
+
+```html
+<div data-sparkline="true" data-points="15,3,17,15,7,11,11,5,0,16,1,13,1,12,13,8,16,4,0,0" data-width="200" data-height="30" data-colors="white" data-stroke-width="3" data-type="line"></div>
+<div data-sparkline="true" data-points="3,8,0,14,11,0,7,1,15,15,20,12,8,13,13,5,1,12,1,11" data-width="200" data-height="30" data-colors="gray" data-stroke-width="1" data-type="line"></div>
+<div data-sparkline="true" data-points="2,16,8,18,2,11,19,12,7,12,12,15,0,0,18,1,7,18,1,13" data-width="200" data-height="30" data-colors="yellow" data-stroke-width="2" data-type="line"></div>
+```
+
+## Pie charts
+
+| Attribute | Description | Default Value |
+|-----------|-------------|---------------|
+| data-sparkline | If sparkline is enabled on this element | None |
+| data-points | List of numbers / data | [] |
+| data-width | Width of the container in px | 100 |
+| data-height | Height of the container in px | 30 |
+| data-colors | List of colors of the slices in HEX or color name | gray |
+
+```html
+<div data-sparkline="true" data-points="10,16,8,18,5,11" data-width="40" data-height="40" data-colors="blue,brown,aqua,yellow" data-type="pie"></div>
+<div data-sparkline="true" data-points="11,14,20,21,37" data-width="40" data-height="40" data-colors="indigo,silver,linen,khaki,crimson" data-type="pie"></div>
+<div data-sparkline="true" data-points="20,44,23" data-width="40" data-height="40" data-colors="olive,green,#FFB6C1" data-type="pie"></div>
+```
---
+++ sparklines.js
@@ -0,0 +1,121 @@
+(() => {
+  const sparklines = document.querySelectorAll('[data-sparkline="true"]');
+  sparklines.forEach(sparkline => {
+    const width = parseInt(sparkline.dataset.width, 10) || 100;
+    const height = parseInt(sparkline.dataset.height, 10) || 30;
+    const gap = parseInt(sparkline.dataset.gap, 10) || 5;
+    const strokeWidth = parseInt(sparkline.dataset.strokeWidth, 10) || 2;
+    const type = sparkline.dataset.type || 'bar';
+    let colors = sparkline.dataset.colors || ['gray'];
+    let points = sparkline.dataset.points || null;
+
+    if (!Array.isArray(colors)) {
+      colors = sparkline.dataset.colors.split(',');
+    }
+
+    // Return if no data is provided.
+    if (!points) { return }
+
+    // Convert to integer point array.
+    points = points.split(',').map(item => parseInt(item, 10));
+
+    // Create SVG sparkline.
+    var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.setAttribute('width', width);
+    svg.setAttribute('height', height);
+
+    function bar(sparkline, svg, points, width, height, colors, gap) {
+      const columnWidth = (gap/points.length) + (width/points.length) - gap;
+      const maxValue = Math.max(...points);
+      points.forEach((point, idx) => {
+        const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        const rectHeight = (point/maxValue)*height;
+        rect.setAttribute('x', (idx*columnWidth) + (idx*gap));
+        rect.setAttribute('y', height-rectHeight);
+        rect.setAttribute('width', columnWidth);
+        rect.setAttribute('height', rectHeight);
+        rect.setAttribute('fill', colors[0]);
+
+        const title = document.createElementNS('http://www.w3.org/2000/svg', 'title');
+        title.textContent = point;
+        rect.appendChild(title);
+
+        svg.appendChild(rect);
+      });
+      sparkline.appendChild(svg);
+    }
+
+    function line(sparkline, svg, points, width, height, colors, strokeWidth) {
+      const spacing = width/(points.length-1);
+      const maxValue = Math.max(...points);
+      const pointsCoords = [];
+      points.forEach((point, idx) => {
+        const maxHeight = (point/maxValue)*height;
+        const x = idx*spacing;
+        const y = height-maxHeight;
+        pointsCoords.push(`${x},${y}`);
+      });
+
+      const line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline');
+      line.setAttribute('points', pointsCoords.join(' '));
+      line.setAttribute('fill', 'none');
+      line.setAttribute('stroke-width', strokeWidth);
+      line.setAttribute('stroke', colors[0]);
+      svg.appendChild(line);
+
+      sparkline.appendChild(svg);
+    }
+
+    function pie(sparkline, svg, points, width, height, colors, gap) {
+      const radius = Math.min(width, height) / 2;
+      const centerX = width / 2;
+      const centerY = height / 2;
+      const total = points.reduce((acc, val) => acc + val, 0);
+      const gapRadians = (gap / radius); // Convert gap size to radians
+      let startAngle = 0;
+
+      points.forEach((point, idx) => {
+        const sliceAngle = (point / total) * 2 * Math.PI;
+        const endAngle = startAngle + sliceAngle;
+
+        const x1 = centerX + radius * Math.cos(startAngle);
+        const y1 = centerY + radius * Math.sin(startAngle);
+        const x2 = centerX + radius * Math.cos(endAngle);
+        const y2 = centerY + radius * Math.sin(endAngle);
+
+        let color = colors[idx % colors.length];
+
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        const largeArcFlag = sliceAngle > Math.PI ? 1 : 0;
+        const d = `M ${centerX},${centerY} L ${x1},${y1} A ${radius},${radius} 0 ${largeArcFlag} 1 ${x2},${y2} Z`;
+        path.setAttribute('d', d);
+        path.setAttribute('fill', color);
+
+        const title = document.createElementNS('http://www.w3.org/2000/svg', 'title');
+        title.textContent = `${(point/total*100).toFixed(2)}%`;
+        path.appendChild(title);
+
+        svg.appendChild(path);
+        startAngle = endAngle;
+      });
+
+      sparkline.appendChild(svg);
+    }
+
+
+    switch (type) {
+    case 'bar': {
+      bar(sparkline, svg, points, width, height, colors, gap);
+      break;
+    }
+    case 'line': {
+      line(sparkline, svg, points, width, height, colors, strokeWidth);
+      break;
+    }
+    case 'pie': {
+      pie(sparkline, svg, points, width, height, colors, gap);
+      break;
+    }
+    }
+  });
+})();
